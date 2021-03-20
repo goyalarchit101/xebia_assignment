7822d54cce46833768a6c5599db859e376ecbef5
goyalarchit101 <ranadheer@example.com>
2021-03-20 20:23:34 +0530

    first commit

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const _ = require('lodash');
+
+const User = mongoose.model('User');
+
+module.exports.register = (req, res, next) => {
+    var user = new User();
+    user.fullName = req.body.fullName;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.save((err, doc) => {
+        if (!err) {
+            if (validateEmail(doc.email)) {
+                res.send(doc);//response is ok
+            }
+            else {
+                res.status(422).send(['email id is not valid']);
+            }
+        }
+        else {
+            if (err.code == 11000)
+                res.status(422).send(['Duplicate email adrress found.']);
+            else
+                return next(err);
+        }
+
+    });
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+module.exports.authenticate = (req, res, next) => {
+    // call for passport authentication
+    passport.authenticate('local', (err, user, info) => {
+        // error from passport middleware
+        if (err) return res.status(400).json(err);
+        // registered user
+        else if (user) return res.status(200).json({ "token": user.generateJwt() });
+        // unknown user or wrong password
+        else return res.status(404).json(info);
+    })(req, res);
+}
+
+module.exports.userProfile = (req, res, next) => {
+    User.findOne({ _id: req._id },
+        (err, user) => {
+            if (!user)
+                return res.status(404).json({ status: false, message: 'User record not found.' });
+            else
+                return res.status(200).json({ status: true, user: _.pick(user, ['fullName', 'email']) });
+        }
+    );
+}
